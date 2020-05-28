dccfa8cfc19d60d4b712a3726af98de97b9b1b61
Davi <31361180+DaviSRodrigues@users.noreply.github.com>
2020-05-28 20:19:05 -0300

    Atualização
    - Atualização com os dados de 28/05/2020
    - Ajustes para que o GitHub Actions rode uma vez por dia às 21h (não mais a cada push)

--- executar-notebooks.sh
+++ executar-notebooks.sh
@@ -4,7 +4,7 @@ set -e
 for file in *.ipynb
 do
 	if papermill --kernel python3 "${file}" "${file}"; then
-        echo "${file} foi executado com sucesso.\n\n\n\n"
+		echo "${file} foi executado com sucesso.\n\n"
     else
         echo "Erro ao executar ${file}"
     fi
