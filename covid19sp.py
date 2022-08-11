5bbe9f1cf91b3bae9b0dfe8e247c33ee7a734091
Davi <31361180+DaviSRodrigues@users.noreply.github.com>
2022-08-11 18:40:07 -0300

    Atualização
    Atualização

--- covid19sp.py
+++ covid19sp.py
@@ -3289,8 +3289,10 @@ def atualiza_service_worker(dados_estado):
 
 
 if __name__ == '__main__':
-    data_processamento = datetime.now()
-    processa_doencas = True
+    for i in range(27, -1, -1):
+        data_processamento = datetime.now() - timedelta(days=i)
+        processa_doencas = False
+        print(f'\n\nData atual -> {data_processamento:%d/%m/%Y}\n\n')
 
-    main()
+        main()
 
